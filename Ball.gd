6ff9479fc8f9f51a81bfce32c2f581b9900e9a55
Mornheo <alvaromofi@gmail.com>
2020-07-05 01:59:28 +0200

    Version 1.0

--- Ball.gd
+++ Ball.gd
@@ -11,7 +11,7 @@ var yspeed = 100
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	apply_impulse(Vector2.ZERO,Vector2.RIGHT*200)
+	apply_impulse(Vector2.ZERO,Vector2.RIGHT*400+Vector2.UP*200)
 	#linear_velocity[0] = xspeed
 	#linear_velocity[1] = yspeed
 	angular_velocity = 0
@@ -24,13 +24,13 @@ func _ready():
 
 
 func _on_Ball_body_entered(body):
-	print_debug(body.name)
+	print_debug(body.name.substr(0,5))
 	pass # Replace with function body.
 
 
 func _on_Ball_body_exited(body):
-	if can_destroy == 1 and body.name == "Paleta":
-		can_destroy = 0
+	if can_destroy == 1 and body.name.substr(0,5) == "Brick":
+		#can_destroy = 0
 		body.queue_free()
 		var ball = load("res://Ball.tscn").instance()
 		ball.global_transform = global_transform
