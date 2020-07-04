3fc6ac4b670efdf8868a7f0497fecd9a282204d7
Mornheo <alvaromofi@gmail.com>
2020-07-05 01:12:53 +0200

    WORLD

--- Paleta.gd
+++ Paleta.gd
@@ -5,7 +5,7 @@ var speed = 8
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	#global_position = Vector2(ProjectSettings.get_setting("display/window/size/width")/8,ProjectSettings.get_setting("display/window/size/height")/2)
-	print_debug(ProjectSettings.get_setting("display/window/size/height"))
+	#print_debug(ProjectSettings.get_setting("display/window/size/height"))
 	pass # Replace with function body.
 	
 func _process(delta):
--- Paleta2.gd
+++ Paleta2.gd
@@ -4,8 +4,8 @@ var speed = 8
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	#global_position = Vector2(ProjectSettings.get_setting("display/window/size/width")/2,ProjectSettings.get_setting("display/window/size/height")/2)
-	print_debug(ProjectSettings.get_setting("display/window/size/height"))
+	#global_position = Vector2(ProjectSettings.get_setting("display/window/size/width")/8,ProjectSettings.get_setting("display/window/size/height")/2)
+	#print_debug(ProjectSettings.get_setting("display/window/size/height"))
 	pass # Replace with function body.
 	
 func _process(delta):
