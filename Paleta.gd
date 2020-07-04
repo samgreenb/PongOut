e7affaa08e85a99fd1ed69652897b3e6a9f5515d
Sam Hersel <gdiarionline@gmail.com>
2020-07-05 01:20:52 +0200

    Merge commit

--- Paleta.gd
+++ Paleta.gd
@@ -4,12 +4,12 @@ var speed = 8
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	#global_position = Vector2(ProjectSettings.get_setting("display/window/size/width")/8,ProjectSettings.get_setting("display/window/size/height")/2)
-	#print_debug(ProjectSettings.get_setting("display/window/size/height"))
+	global_position = Vector2(ProjectSettings.get_setting("display/window/size/width")/8,ProjectSettings.get_setting("display/window/size/height")/2)
+	print_debug(ProjectSettings.get_setting("display/window/size/height"))
 	pass # Replace with function body.
 	
 func _process(delta):
-	if(Input.is_action_pressed("j1_up")):
+	if(Input.is_action_pressed("j2_up")):
 		move_and_collide(Vector2.UP*speed)
-	if(Input.is_action_pressed("j1_down")):
+	if(Input.is_action_pressed("j2_down")):
 		move_and_collide(Vector2.DOWN*speed)
